5d6bd395f81f047655d1ec0f3cc62c7de973a124
liri0 <haoliri0@163.com>
2016-07-02 00:26:32 +0800

    add some notes

--- matlab/control.m
+++ matlab/control.m
@@ -1,29 +1,33 @@
-% pause(8)
-% % 请在开始运行程序6秒后开始播放未命名.avi
-% 
-% for i = 1:8
-%     main_stewart(0,0,400,2*(i),0,0);
-%     pause(0.01);
-% end
-% pause(1.2)
-% for i = 1:8
-%     main_stewart(0,0,400,2*(8-i),0,0);
-%     pause(0.01);
-% end
-% 
-% pause(0.4)
-% 
-% for i = 1:8
-%     main_stewart(0,0,400,0,2*(i),0);
-%     pause(0.01);
-% end
-% 
-% pause(0.6)
-% 
-% for i = 1:8
-%     main_stewart(0,0,400,0,2*(8-i),0);
-%     pause(0.01);
-% end
+pause(8) %pause(8)表示程序在此等待8秒
+% 请在开始运行程序6秒后开始播放未命名.avi
+
+for i = 1:8 %分8次运行到指定位姿(0,0,400,16,0,0)
+    main_stewart(0,0,400,2*(i),0,0);
+    pause(0.02); %相邻两次运行间隔为0.02秒(20ms)
+end
+
+pause(1.2)  %等待1.2秒
+
+for i = 1:8 %逐渐恢复到初始位姿(0,0,400,0,0,0)
+    main_stewart(0,0,400,2*(8-i),0,0);
+    pause(0.02);
+end
+
+%%下面部分的代码类似
+
+pause(0.4)
+
+for i = 1:8
+    main_stewart(0,0,400,0,2*(i),0);
+    pause(0.02);
+end
+
+pause(0.6)
+
+for i = 1:8
+    main_stewart(0,0,400,0,2*(8-i),0);
+    pause(0.02);
+end
 % 
 % pause(0.4)
 % 
@@ -39,6 +43,9 @@
 % 
 % pause(0.4)
 % 
+
+
+
 % for i = 1:8
 %     main_stewart(0,0,400,0,3*i,0);
 %     pause(0.01);
@@ -62,40 +69,3 @@
 %     pause(0.01);
 % end
 % 
-
-% pause(0.01);
-% main_stewart(0,0,400,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,410,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,420,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,430,0,0,0);
-% pause(0.01)
-% main_stewart(0,0,440,0,0,0);
-% pause(0.01)
-% main_stewart(0,0,450,0,0,0);
-% pause(0.01)
-% main_stewart(0,0,460,0,0,0);
-% pause(0.01)
-% main_stewart(0,0,470,0,0,0);
-% pause(0.01)
-% main_stewart(0,0,480,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,470,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,460,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,450,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,440,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,430,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,420,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,410,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,400,0,0,0);
-% pause(0.01);
-% main_stewart(0,0,390,0,0,0);
